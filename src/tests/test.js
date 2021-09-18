aca64f162400518d7626608ea46ce0517797e6b8
lbrasca <lucianobrasca@gmail.com>
2021-09-18 20:55:34 -0300

    ajustes tests

--- src/tests/test.js
+++ src/tests/test.js
@@ -4,7 +4,7 @@ var chai = require('chai');
 var expect = chai.expect;
 const app = require('../server');
 
-describe('Creacion de usuario', function() {
+describe('Creacion de usuario', () => {
 
     it('Deberia retornar error por tener una contraseña con longitud < a 8', done =>{
         request(app)
@@ -18,7 +18,7 @@ describe('Creacion de usuario', function() {
                 preferedCurrency: "usd"
             })
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .end(function(err, res) {
+            .end((err, res) => {
                 console.log(res.body);
                 expect(res.statusCode).to.be.equal(400);
                 done();
@@ -37,7 +37,7 @@ describe('Creacion de usuario', function() {
                 preferedCurrency: "aaa"
             })
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .end(function(err, res) {
+            .end((err, res) => {
                 console.log(res.body);
                 expect(res.statusCode).to.be.equal(400);
                 done();
@@ -46,7 +46,7 @@ describe('Creacion de usuario', function() {
 
   });
 
-describe('Login', function() {
+describe('Login', () => {
 
     it('Deberia retornar, junto con los datos del usuario, el token de acceso.', done =>{
         request(app)
@@ -54,7 +54,7 @@ describe('Login', function() {
             .set('Accept', 'application/json')
             .send({ username: "usuario10", password: "123qweasd"})
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .end(function(err, res) {
+            .end((err, res) => {
                 expect(res.body.token).to.be.an('string');
                 expect(res.statusCode).to.be.equal(200);
                 done();
@@ -64,26 +64,26 @@ describe('Login', function() {
 
   });
 
-describe('Obtengo las criptomonedas disponibles', function() {
+describe('Obtengo las criptomonedas disponibles', () => {
 
     var token = null;
   
-    before(function(done) {
+    before((done) => {
       request(app)
         .post('/users/login')
         .set('Accept', 'application/json')
         .send({ username: "usuario10", password: "123qweasd"})
-        .end(function(err, res) {
+        .end((err, res) => {
           token = res.body.token; 
           done();
         });
     });
   
-    it('Deberia recuperar un array con todas las criptomonedas disponibles', function(done) { 
+    it('Deberia recuperar un array con todas las criptomonedas disponibles', (done) => { 
       request(app)
         .get('/cryptos/getAll')
         .set('Authorization', 'Bearer ' + token)
-        .end(function(err, res) {
+        .end((err, res) => {
             expect(res.body).to.be.an('array');
             expect(res.statusCode).to.be.equal(200);
             done();
@@ -95,37 +95,37 @@ describe('Obtengo el top N de monedas del usuario', function() {
 
     var token = null;
   
-    before(function(done) {
+    before((done) => {
       request(app)
         .post('/users/login')
         .set('Accept', 'application/json')
         .send({ username: "usuario10", password: "123qweasd"})
-        .end(function(err, res) {
+        .end((err, res) => {
           token = res.body.token; 
           done();
         });
     });
     
-    it('Deberia retornar error por tener N mayor a 25', function(done) { 
+    it('Deberia retornar error por tener N mayor a 25', (done) => { 
       request(app)
         .post('/users/getTopNCryptos')
         .set('Accept', 'application/json')
         .set('Authorization', 'Bearer ' + token)
         .send({ n: 26})
-        .end(function(err, res) {
+        .end((err, res) => {
             console.log(res.body);
             expect(res.statusCode).to.be.equal(400);
             done();
         });
     });
 
-    it('Deberia retornar error por tener un valor de order no admitido', function(done) { 
+    it('Deberia retornar error por tener un valor de order no admitido', (done) => { 
         request(app)
           .post('/users/getTopNCryptos')
           .set('Accept', 'application/json')
           .set('Authorization', 'Bearer ' + token)
           .send({ n: 25, order: "asce"})
-          .end(function(err, res) {
+          .end((err, res) => {
               console.log(res.body);
               expect(res.statusCode).to.be.equal(400);
               done();
@@ -133,3 +133,36 @@ describe('Obtengo el top N de monedas del usuario', function() {
       });
 
   });
+
+  describe('Verifico que requieran token los endpoints que lo utilizan', () =>{
+    it('El endpoint /cryptos/getAll deberia retornar error 401 al no enviarle el token', (done) => {
+      request(app)
+        .get('/cryptos/getAll')
+        .set('Accept', 'application/json')
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(401);
+          done();
+        })
+    });
+
+    it('El endpoint /users/addCryptocurrency deberia retornar error 401 al no enviarle el token', (done) => {
+      request(app)
+        .post('/users/addCryptocurrency')
+        .set('Accept', 'application/json')
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(401);
+          done();
+        })
+    });
+
+    it('El endpoint /users/getTopNCryptos deberia retornar error 401 al no enviarle el token', (done) => {
+      request(app)
+        .post('/users/getTopNCryptos')
+        .set('Accept', 'application/json')
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(401);
+          done();
+        })
+    });
+
+  })
